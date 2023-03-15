632f7a857341a94960f61ce1fc2be2eb00990633
Shahzaib KK <shahzaib84.sk@gmail.com>
2023-03-15 09:57:54 +0500

    going Hard

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Topic
+from .forms import NewTopic
+from django.http import HttpRequest
+
+# from django.db.models.query import QuerySet
 
 
 # Create your views here.
@@ -12,3 +16,23 @@ def topics(req):
     topics = Topic.objects.order_by("date")
     context = {"topics": topics}
     return render(req, "learning_logs/topics.html", context)
+
+
+def topic(req, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+    entires = topic.entry_set.order_by("-date_added")
+    context = {"topic": topic, "entries": entires}
+    return render(req, "learning_logs/topic.html", context)
+
+
+def new_topic(req: HttpRequest):
+    if req.method != "POST":
+        form = NewTopic()
+    else:
+        form = NewTopic(data=req.POST)
+        form.is_valid()
+        form.save()
+        return redirect("learning_logs:topics")
+
+    context = {"form": form}
+    return render(req, "learning_logs/new_topic.html", context)
